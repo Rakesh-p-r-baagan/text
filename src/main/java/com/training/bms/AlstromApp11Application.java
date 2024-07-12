2714659878e7e0a0fff015a4405012e2c92351f2
rakeshpr <baagan8@gmail.com>
2024-07-12 13:07:27 +0530

    Initial commit

---
+++ src/main/java/com/training/bms/AlstromApp11Application.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstromApp11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstromApp11Application.class, args);
+		System.out.println("hello");
+	}
+
+}
